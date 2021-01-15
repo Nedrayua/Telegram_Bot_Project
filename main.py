511a90f886bf0139d64769087a4a4ae72f2b43bf
Nedrayua <nedrayua@gmail.com>
2021-01-15 20:24:40 +0200

    modified main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ me.connect('SHOP')
 
 bot.remove_webhook()
 time.sleep(0.5)
-bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pen'))
+bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
 
 
 # app.run(debug=True)
