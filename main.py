41b8bffaf21573d24a9f0123a4c26e43b5a78149
Nedrayua <nedrayua@gmail.com>
2021-01-15 20:55:29 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
-from shop.bot.shop_bot import bot
+from shop.bot.shop_bot import bot, app
 from shop.models.shop_models import *
-from shop.api.app_restful import app
+from shop.api.app_restful import app as app2
 
 from shop.bot.config import WEBHOOK_URL
 
@@ -12,6 +12,6 @@ time.sleep(0.5)
 bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
 
 
-# app.run(debug=True)
+app.run(debug=True)
 
-
+# app2.run(debug=True)
